8807b80b6076970658fdab2e2771089924e32bc9
dawit belay <dawitbelayg@gmail.com>
2025-12-15 15:00:52 +0300

    first commit....seting up the project and add prestiles and public asset

---
+++ src/app.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <main>
+        <div>
+            <h1 className="text-3xl">Welcome to My 3D Portfolio!</h1>
+        </div>
+    </main>
+  )
+}
+export default App
