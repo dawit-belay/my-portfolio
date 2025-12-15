f96f0a57223fa4b871d1430c4461f23940a106ce
dawit belay <dawitbelayg@gmail.com>
2025-12-15 16:45:27 +0300

    applying left side hero section

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,9 @@
+import Hero from "./sections/Hero"
 const App = () => {
   return (
-    <main>
-        <div>
-            <h1 className="text-3xl">Welcome to My 3D Portfolio!</h1>
-        </div>
-    </main>
+    <>
+     <Hero /> 
+    </>
   )
 }
 export default App
