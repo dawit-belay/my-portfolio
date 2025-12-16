f91589e785c15637836a6cffb7d633e25fc242c9
dawit belay <dawitbelayg@gmail.com>
2025-12-16 13:52:43 +0300

    adding showcase section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -5,7 +5,9 @@ import HeroExperience from "../components/models/hero_models/HeroExperience";
 const Hero = () => {
   return (
     <section id="hero" className="relative overflow-hidden">
-
+      <div className="absolute top-0 left-0 z-10">
+        <img src="/images/bg.png" alt="" />
+      </div>
       <div className="hero-layout">
         {/* LEFT: Hero Content */}
         <header className="flex flex-col justify-center md:w-full w-screen md:px-20 px-5">
